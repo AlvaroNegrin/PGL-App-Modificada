c2f0d01d0c9be5afd24867b391f1d3b4db2bd48d
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-10-19 21:18:13 +0100

    refact(): Move the style to a separate class and move the top of the app to a component Header

---
+++ app-modificada/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes, { string } from 'prop-types'
+import { View, Text, Pressable, Button } from 'react-native'
+import { styles } from '../styles/MainStyles'
+
+type HeaderProps = {
+    title: string
+    onPressInfo: () => void
+    onPressRepo: () => void
+}
+
+export const Header = (props: HeaderProps) => {
+    const {title, onPressInfo, onPressRepo} = props
+  return (
+      <View style={styles.topContainer}>
+        <Text style={styles.firsttoprowContainer}>{title}</Text>
+        <View style={styles.rowTopSecondContainer}>
+        <Pressable style={styles.buttonruta} onPress={onPressInfo}>
+          <Text style={{...{color: 'white', fontWeight: 'bold', textTransform: 'uppercase'}, ...styles.shadoxboxing}}>Mi info</Text>
+        </Pressable>
+        <Button onPress={onPressRepo} title="Mi Repo" color="light-gray" accessibilityLabel='Un botón pal QR' />
+        </View>
+      </View>
+      
+  )
+}
+
+export default Header
