cd6505d25d3612eb5b7e4ca3dc0f9b130f44ac36
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-10-20 12:25:02 +0100

    feat(): Create class of color themes

---
+++ app-modificada/styles/ColorThemes.ts
@@ -0,0 +1,23 @@
+export const lightTheme = {
+    backgroundColor: '#fff',
+    textColor: '#000',
+    headerBackground: 'gray',
+    buttonBackground: 'darkgray',
+    buttonTextColor: 'white',
+    borderColor: 'black',
+    avatarBorderColor: 'gray',
+    itemBackgroundColor: 'silver',
+    itemTextColor: 'darkred',
+  };
+  
+export const darkTheme = {
+    backgroundColor: '#121212',
+    textColor: '#fff',
+    headerBackground: '#333',
+    buttonBackground: '#444',
+    buttonTextColor: '#fff',
+    borderColor: '#fff',
+    avatarBorderColor: '#fff',
+    itemBackgroundColor: '#555',
+    itemTextColor: '#e0e0e0',
+  };
